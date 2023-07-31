1dd952beb81d88973ba4af4d9e54ecefc362009f
NicasioTn <55986701+NicasioTn@users.noreply.github.com>
2023-08-01 00:09:29 +0700

    Update main.py

--- lib/main.py
+++ lib/main.py
@@ -343,7 +343,8 @@ class PasswordEvaluation(QDialog):
             time_parts.append(f"{minutes} minute{'s' if minutes != 1 else ''}")
         if seconds > 0:
             time_parts.append(f"{seconds} second{'s' if seconds != 1 else ''}")
-
+        if years > 10:
+            time_parts = ['more than 10 years']
         return ", ".join(time_parts)
     
 import hashlib
